b9b1ecccf1c3338f843abc0e1005afd6ed0703dd
trottomv <trottomv@gmail.com>
2016-03-14 15:46:56 +0100

    botest upgrade

--- lib/botest.rb
+++ lib/botest.rb
@@ -6,7 +6,7 @@ require 'feed-normalizer'
 require 'open-uri'
 require 'simple-rss'
 require 'open-uri'
-require 'curb'
+# require 'curb'
 
 class Botest
 Dotenv.load
@@ -34,45 +34,36 @@ Telegram::Bot::Client.run(token) do |bot|
     when '/lastxkcd'
       bot.api.send_message(chat_id: message.chat.id, text: "#{feed.entries.first.url}")
     end
+    def rssCal
+      rsscal = SimpleRSS.parse open('#{ENV[FEED_CAL]}')
+      timefeed=rsscal.items.first.pubDate.strftime ("%d-%m-%Y %H")
+      nowh=Time.now.strftime ("%d-%m-%Y %H")
+      case timefeed
+        when nowh
+        # http = Curl.get("https://api.telegram.org/bot#{ENV['TOKEN']}/sendMessage?chat_id=#{ENV['CHAT_ID']}&text=#{rss.items.first.title}")
+        bot.api.send_message(chat_id: message.chat.id, text: "#{rsscal.items.first.title}")
+      end
+    end
+    def rssXkcd
+      rssXcd = SimpleRSS.parse open('http://xkcd.com/rss.xml')
+      timefeed=rss.items.first.pubDate.strftime ("%d-%m-%Y")
+      today=Time.now.strftime ("%d-%m-%Y")
+      case timefeed
+      when today
+        # http = Curl.get("https://api.telegram.org/bot#{ENV['TOKEN']}/sendMessage?chat_id=#{ENV['CHAT_ID']}&text=#{rss.items.first.link}")
+        bot.api.send_message(chat_id: message.chat.id, text: "#{rssXcd.items.first.link}")
+      end
+    end
+    def rssturnoffus
+      rssreturnoffus = SimpleRSS.parse open('http://turnoff.us/feed.xml')
+      timefeed=rss.items.first.pubDate.strftime ("%d-%m-%Y %H")
+      nowh=Time.now.strftime ("%d-%m-%Y %H")
+      case timefeed
+      when nowh
+        # http = Curl.get("https://api.telegram.org/bot#{ENV['TOKEN']}/sendMessage?chat_id=#{ENV['CHAT_ID']}&text=#{rss.items.first.link}")
+        bot.api.send_message(chat_id: message.chat.id, text: "#{rssturnoffus.items.first.link")
+      end
+    end
   end
-end
-end
-
-def rssCal
-Dotenv.load
-
-rsscal = SimpleRSS.parse open('#{ENV[FEED_CAL]}')
-
-timefeed=rsscal.items.first.pubDate.strftime ("%d-%m-%Y %H")
-nowh=Time.now.strftime ("%d-%m-%Y %H")
-case timefeed
- when nowh
-  http = Curl.get("https://api.telegram.org/bot#{ENV['TOKEN']}/sendMessage?chat_id=#{ENV['CHAT_ID']}&text=#{rss.items.first.title}")
-end
-end
-
-def rssXkcd
-Dotenv.load
-
-rss = SimpleRSS.parse open('http://xkcd.com/rss.xml')
-
-timefeed=rss.items.first.pubDate.strftime ("%d-%m-%Y")
-today=Time.now.strftime ("%d-%m-%Y")
-case timefeed
- when today
-  http = Curl.get("https://api.telegram.org/bot#{ENV['TOKEN']}/sendMessage?chat_id=#{ENV['CHAT_ID']}&text=#{rss.items.first.link}")
-end
-end
-
-def rssturnoffus
-Dotenv.load
-
-rss = SimpleRSS.parse open('http://turnoff.us/feed.xml')
-
-timefeed=rss.items.first.pubDate.strftime ("%d-%m-%Y %H")
-nowh=Time.now.strftime ("%d-%m-%Y %H")
-case timefeed
- when nowh
-  http = Curl.get("https://api.telegram.org/bot#{ENV['TOKEN']}/sendMessage?chat_id=#{ENV['CHAT_ID']}&text=#{rss.items.first.link}")
 end
 end
